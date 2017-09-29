97b45c8bbda113cd73ff4134618f51d681119440
kash <krswshnt@gmail.com>
2017-09-30 01:41:51 +0900

    import orders module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersModule } from './orders/orders.module';
+
+import { BgImgDirective } from './shared/bg-img.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BgImgDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OrdersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
