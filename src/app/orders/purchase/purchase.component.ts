52bd80b50065462946001620b611e62194c37a7e
kash <krswshnt@gmail.com>
2017-09-30 01:36:07 +0900

    add order purchase component

---
+++ src/app/orders/purchase/purchase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase',
+  templateUrl: './purchase.component.html',
+  styleUrls: ['./purchase.component.styl']
+})
+export class PurchaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
