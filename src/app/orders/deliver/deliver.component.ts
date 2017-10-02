c9f61642598d8f8883977ebe17eb8ab6c1a35b02
kash <krswshnt@gmail.com>
2017-10-02 14:13:11 +0900

    add deliver component

---
+++ src/app/orders/deliver/deliver.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deliver',
+  templateUrl: './deliver.component.html',
+  styleUrls: ['./deliver.component.styl']
+})
+export class DeliverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
