84e02087c1cea0959a7033b224070ddd1014bfcd
kash <krswshnt@gmail.com>
2017-10-06 10:24:58 +0900

    implement api request

--- src/app/orders/shared/orders.service.ts
+++ src/app/orders/shared/orders.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { environment } from '../../../environments/environment';
 
 import { Product } from './product.model';
-import { Order } from './order.model';
+import { Order, OrderDetailsAttribute } from './order.model';
 
 @Injectable()
 export class OrdersService {
@@ -14,27 +14,43 @@ export class OrdersService {
 
   // 全商品を取得
   fetchProducts () : Observable<Product[]> {
-    const requestUrl = this.genUrl('products/');
+    const requestUrl = this.makeUrl('products/');
     return this.http.get<Product[]>(requestUrl);
   }
 
   // 会計前のリストを取得
   fetchBeforePurchaseOrders () : Observable<Order[]> {
-    const requestUrl = this.genUrl(`groups/${environment.group.id}/purchase`);
+    const requestUrl = this.makeUrl(`groups/${environment.group.id}/purchase`);
     return this.http.get<Order[]>(requestUrl);
   }
 
   // 配達前のリスト
   fetchBeforeDeliverOrders () : Observable<Order[]> {
-    const requestUrl = this.genUrl(`groups/${environment.group.id}/deliver`);
+    const requestUrl = this.makeUrl(`groups/${environment.group.id}/deliver`);
     return this.http.get<Order[]>(requestUrl);
   }
-  registerOrder (body: Order) : Observable<Order> {
-    const requestUrl = this.genUrl(`group/${environment.group.id}/register_orders`)
-    return this.http.post<Order>(requestUrl, body)
+
+  registerOrder (orderDetails: OrderDetailsAttribute[]) : Observable<Order> {
+    const requestUrl = this.makeUrl(`groups/${environment.group.id}/register_orders`);
+    const body = {
+      order: {
+        orderDetailsAttributes: orderDetails.map ((orderItem) => orderItem.makeRequestBody)
+      }
+    };
+    return this.http.post<Order>(requestUrl, body);
   }
 
-  private genUrl(url : string) : string {
+  purchaseOrder (id: number) : Observable<Order> {
+    const requestUrl = this.makeUrl(`gruops/${environment.group.id}/purchase/${id}/commit`);
+    return this.http.post<Order>(requestUrl, {});
+  }
+
+  deliverOrder (id: number) : Observable<Order> {
+    const requestUrl = this.makeUrl(`groups/${environment.group.id}/deliver/${id}/commit`);
+    return this.http.post<Order>(requestUrl, {});
+  }
+
+  private makeUrl(url : string) : string {
     return `http://${environment.api.host}:${environment.api.port}/api/${url}`;
   }
 }
