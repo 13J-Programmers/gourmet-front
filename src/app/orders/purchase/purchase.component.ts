a87387ea9af4bde1a941f34a1f956047f4d6c059
kash <krswshnt@gmail.com>
2017-10-11 09:54:03 +0900

    implement purchase

--- src/app/orders/purchase/purchase.component.ts
+++ src/app/orders/purchase/purchase.component.ts
@@ -13,15 +13,56 @@ export class PurchaseComponent implements OnInit {
   constructor(private ordersService: OrdersService) { }
 
   public beforePurchaseOrders: Order[];
+  public currentOrder: Order | null;
+  public currentOrderStyles: {};
 
   ngOnInit() {
+    this.fetchBeforePurchaseOrders();
+  }
+
+  fetchBeforePurchaseOrders() {
     this.ordersService.fetchBeforePurchaseOrders()
       .subscribe(res => {
         this.beforePurchaseOrders = res;
+        this.currentOrder = this.beforePurchaseOrders.length > 0 ?
+          this.beforePurchaseOrders[0] : null;
       });
   }
 
-  purchaseOrder (order: Order) {
-    // this.ordersService.purchaseOrder(order)
+  purchaseOrder (orderId: number) {
+    this.ordersService.purchaseOrder(orderId)
+      .subscribe(res => {
+        this.fetchBeforePurchaseOrders();
+      });
   }
+
+  calcPrice(order: Order): number {
+    return order.order_details.map(orderItem => {
+      return orderItem.amount * orderItem.quantity
+    }).reduce((prev, current) => {
+      return prev + current;
+    }, 0);
+  }
+
+  private touchX: number = 0;
+  private startX: number = 0;
+  private isRelease: Boolean = false;
+  onTouchStart(event) {
+    this.startX = event.changedTouches[0].pageX;
+    this.isRelease = false;
+  }
+  onTouchEnd(event) {
+    if (event.changedTouches[0].pageX - this.startX > 120) {
+      this.purchaseOrder(this.currentOrder.id);
+    }
+    this.currentOrderStyles = { 'left.px': 0 };
+    this.isRelease = true;
+  }
+  onTouchMove(event) {
+    const left = event.changedTouches[0].pageX - this.startX
+    this.currentOrderStyles = {
+      'left.px': left,
+    };
+  }
+
 }
