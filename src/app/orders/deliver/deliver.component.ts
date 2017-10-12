5891ba7b3f7142ee403fa60a36315456285d9ce3
kash <krswshnt@gmail.com>
2017-10-12 20:59:12 +0900

    implement deliver

--- src/app/orders/deliver/deliver.component.ts
+++ src/app/orders/deliver/deliver.component.ts
@@ -13,12 +13,32 @@ export class DeliverComponent implements OnInit {
   constructor(private ordersService: OrdersService) { }
 
   public beforeDeliverOrders: Order[];
+  public currentOrder: Order;
+  public deliveredOrders: Order[] = [];
 
   ngOnInit() {
+    this.fetchBeforeDeliverOrders();
+  }
+
+  fetchBeforeDeliverOrders() {
     this.ordersService.fetchBeforeDeliverOrders()
       .subscribe(res => {
         this.beforeDeliverOrders = res;
-      })
+        this.currentOrder = this.beforeDeliverOrders[0];
+      });
+  }
+  deliverOrder(id: number) {
+    this.ordersService.deliverOrder(id)
+      .subscribe(res => {
+        if (this.deliveredOrders.length > 1) {
+          this.deliveredOrders.shift();
+        }
+        this.deliveredOrders.push(this.currentOrder);
+        this.fetchBeforeDeliverOrders();
+      });
+  }
+  onKeyDown(event) {
+    this.deliverOrder(this.currentOrder.id);
   }
 
 }
