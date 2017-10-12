e4855574945227e8e707251054f9c63b3e42836d
kash <krswshnt@gmail.com>
2017-10-13 03:44:51 +0900

    add price to product model

--- src/app/orders/shared/product.model.ts
+++ src/app/orders/shared/product.model.ts
@@ -3,19 +3,10 @@ export class Product {
   constructor (
     public id: number,
     public name: string,
+    public price: number,
     public soldout: boolean,
     public description: string | null,
     public imageClass: string | null
   ) { }
-
-  public static buildFromApiResponse(obj: any) : Product {
-    return new Product(
-      obj.id,
-      obj.name,
-      obj.soldout,
-      obj.description,
-      obj.image_class
-    );
-  }
 
 }
